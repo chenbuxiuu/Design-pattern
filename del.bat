08f39f9803263ccfa6a04eba9b177aeec3d5990a
cxh-er <1844608972@qq.com>
2018-03-16 10:45:38 +0800

    del.bat Builder Parttern

--- del.bat
+++ del.bat
@@ -1,1 +1,7 @@
-del *.class
+@echo off  
+rem 正在搜索...  
+rem for /f "delims=" %%i in ('dir /b /a-d /s "*.class"') do echo %%i  
+rem 搜索完毕  
+rem 删除
+for /f "delims=" %%i in ('dir /b /a-d /s "*.class"') do del  "%%i" 
+rem 删除完成
